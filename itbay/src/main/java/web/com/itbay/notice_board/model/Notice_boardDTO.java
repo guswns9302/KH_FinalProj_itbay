4d17b664105714181213e73ce84ed394efe27008
jjang <jjang@DESKTOP-E8AAH41>
2022-02-08 11:09:56 +0900

    commit test 05

--- itbay/src/main/java/web/com/itbay/notice_board/model/Notice_boardDTO.java
+++ itbay/src/main/java/web/com/itbay/notice_board/model/Notice_boardDTO.java
@@ -5,8 +5,6 @@ import java.sql.Date;
 public class Notice_boardDTO {
 	private int id;
 	private String subject;
-	private String contents;
-	private String members_id;
 	private Date create_date;
 	private int view_CNT;
 	
