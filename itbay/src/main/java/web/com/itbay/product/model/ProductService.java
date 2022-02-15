7d3228897e5fe7f819921a7cedc69f96a45865e0
guswns9302 <guswns930213@gmail.com>
2022-02-15 10:11:33 +0900

    불피요 컨트롤러 삭제

--- itbay/src/main/java/web/com/itbay/product/model/ProductService.java
+++ itbay/src/main/java/web/com/itbay/product/model/ProductService.java
@@ -5,21 +5,16 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import web.com.itbay.PagingUtil;
 import web.com.itbay.img.model.ImgDAO;
 import web.com.itbay.img.model.ImgDTO;
-import web.com.itbay.product.controller.ProductWriteController;
 
 @Service
 public class ProductService {
